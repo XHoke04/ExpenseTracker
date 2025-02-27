dcc144cefdd564c147ab771b5df9c8101264d13a
Xander <xander59@verizon.net>
2025-02-27 16:42:33 -0500

    Added the following features: add_expense, get_user_expenses, update_expense, and delete_expense.

--- db_operations.py
+++ db_operations.py
@@ -29,3 +29,55 @@ def get_all_users():
     cursor.close()
     conn.close()
     return rows
+
+## Expense Table Operations
+
+def add_expense(user_id, category_id, amount, date, description=None):
+    """
+    Insert a new expense record into the exepnses table.
+
+    :param user_id: ID of the user associated with this expense
+    :param category_id: ID of the category
+    :param amount: Numeric value representing how much was spent
+    :param date: String representing the date of the expense (e.g. '2025-01-05')
+    """
+
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+    query = """
+    INSERT INTO expenses (user_id, category_id, amount, date, description)
+    VALUES (?, ?, ?, ?, ?)
+    """
+    cursor.execute(query, (user_id, category_id, amount, date, description))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Expense added successfully")
+
+def get_user_expense(user_id):
+    """
+    Retrieve all expenses for a given user, most recent first.
+
+    :param user_id: The user for whom to retrieve expenses
+    :return: A list of tuples, each tuple representing one expense record
+            (expense_id, user_id, category_id, amount, date, description)
+    """
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+    query = """
+    SELECT expense_id, user_id, category_id, amount, date, description
+    FROM expenses
+    WHERE user_id = ?
+    ORDER BY date DESC
+    """
+    cursor.execute(query, (user_id,))
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return rows
+
+#def update_expense()
